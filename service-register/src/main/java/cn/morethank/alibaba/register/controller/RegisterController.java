a17dd36fad71041014ab6812637d27d5a1007b4b
expotoker <2402060178@qq.com>
2020-05-27 00:02:58 +0800

    service register demo

---
+++ service-register/src/main/java/cn/morethank/alibaba/register/controller/RegisterController.java
@@ -0,0 +1,15 @@
+package cn.morethank.alibaba.register.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegisterController {
+
+    @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+    public String echo(@PathVariable String string) {
+        return "Hello Nacos Discovery " + string;
+    }
+}
